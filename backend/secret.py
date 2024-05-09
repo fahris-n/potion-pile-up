062ef13e7be006d32e912dcc20d7c308b88946de
Fahris Nassief <fahrisnassief@Fahriss-MacBook-Pro.local>
2024-05-08 22:48:19 -0600

    Deleted cahced secret.py file from repo

--- backend/secret.py
+++
@@ -1,5 +0,0 @@
-# Railway MySQL connection
-DATABASE_URI = 'mysql+pymysql://root:gricmYqtTjmPGjFkTySIXJGtBKBOtpfS@monorail.proxy.rlwy.net:45810/railway'
-
-# Secret key
-SECRET_KEY = '73h9LxK#vDg2Fp$!q'
