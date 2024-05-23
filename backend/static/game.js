24994cc941339a4652f44c4ffd4688f54800acd4
Fahris Nassief <fahrisnassief@Fahriss-MacBook-Pro.local>
2024-05-22 21:06:27 -0600

    Got sprite animations in game

--- backend/static/game.js
+++ backend/static/game.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
+
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 800;
 
@@ -7,11 +8,44 @@ const playerImage = new Image();
 playerImage.src = 'static/media/wizard_shuffle_sheet.png'
 const spriteWidth = 32;
 const spriteHeight = 32;
+let playerState = "moveLeft";
+
+let gameFrame = 0;
+const staggerFrames = 12;
+
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'moveLeft',
+        frames: 3,
+    },
+    {
+        name: 'moveRight',
+        frames: 3,
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let i = 0; i < state.frames; i++) {
+        let positionX = i * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+})
 
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 260, 715, 86, 86)
+
+
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
