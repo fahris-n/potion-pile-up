28e6d5452505a1f0cae0df7caaeec7a1799fd8d6
Fahris Nassief <fahrisnassief@Fahriss-MacBook-Pro.local>
2024-05-28 15:51:09 -0600

    Created potion class to increase performance when animating potion sprites in game

--- backend/static/js/game.js
+++ backend/static/js/game.js
@@ -17,8 +17,8 @@ let gameFrame = 0;
 const staggerFrames = 12;
 
 // Initialize position and velocity variables for player
-let x = 250;
-let y = 715;
+let xPlayer = 250;
+let yPlayer = 715;
 let vxr = 0;
 let vxl = 0;
 
@@ -74,25 +74,7 @@ function setupEventListeners(){
 // Initialize event listeners
 setupEventListeners();
 
-// Character animation loop
-function animate() {
-    x += vxr;
-    x += vxl;
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
-    let frameX = spriteWidth * position;
-    let frameY = spriteAnimations[playerState].loc[position].y;
-    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, x, y, 86, 86)
-
-    gameFrame++;
-    requestAnimationFrame(animate);
-};
-
-// Start character animation
-animate();
-
-// Potion object setup
+// Potion setup
 const potionSpritesheet = new Image();
 potionSpritesheet.src = 'static/media/potions.png';
 
@@ -101,3 +83,83 @@ const potionHeight = 24;
 
 const spriteSheetCols = 16;
 const spriteSheetRows = 10;
+
+// Potion class to manage individual potions
+class Potion {
+    constructor() {
+        this.x = Math.random() * CANVAS_WIDTH;
+        this.y = -30;
+        this.width = 24;
+        this.height = 32;
+        this.spriteX = Math.floor(Math.random() * spriteSheetCols);
+        this.spriteY = Math.floor(Math.random() * spriteSheetRows);;
+        this.speed = 2;
+    }
+
+    draw() {
+        ctx.drawImage(
+            potionSpritesheet,
+            this.spriteX * potionWidth,
+            this.spriteY * potionHeight,
+            potionWidth,
+            potionHeight,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+    }
+
+    update() {
+        this.y += this.speed;
+        if (this.y > CANVAS_HEIGHT) {
+            this.y = -30;
+            this.x = Math.random() * CANVAS_WIDTH;
+            this.spriteX = Math.floor(Math.random() * spriteSheetCols);
+            this.spriteY = Math.floor(Math.random() * spriteSheetRows);
+        }
+    }
+}
+
+// Array to store multiple potions
+let potions = [];
+// Interval between potions in frames
+const potionInterval = 1000;
+
+function addPotion() {
+    potions.push(new Potion());
+}
+
+// Game animation loop
+function animate() {
+    xPlayer += vxr;
+    xPlayer += vxl;
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    // Add new potions at intervals
+    if (gameFrame % potionInterval === 0) {
+        addPotion();
+    }
+
+    // Update and draw each potion
+    potions.forEach((potion) => {
+        potion.update();
+        potion.draw();
+    });
+
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, xPlayer, yPlayer, 86, 86)
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+};
+
+// Start game animation
+animate();
+
+//TODO
+// Tighten potion spread 
+// Add potion caught tracker and potion dropped tracker
+// Change potion fall speed based on potion caught tracker
